a394f57a902af37b0d893acced4dd896cf18bf8c
Horner-J <jasonandrewhorner@gmail.com>
2022-05-26 21:20:51 -0600

    Adding typewriter styling function

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+var i = 0;
+var txt = 'Professional Problem Solver';
+var speed = 75;
+var blank = ''
+var repeater;
+
+
+
+function mouseLeave() {
+    document.getElementById("demo").innerHTML = blank;
+  }
+
+  
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+  setTimeout(2)
+}
